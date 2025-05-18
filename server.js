54ab93630930547d341d5cb822da5a1ca5cc38c8
ihamedr <amirhamedrazavi@gmail.com>
2025-05-18 22:17:30 +0330

    Add files via upload

--- server.js
+++ server.js
@@ -6,15 +6,29 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwVtOq8vLhEh_rkH8vsZ7-TugpHQGHffNOyZM3HtB8VkmXtAw3kxuETgwMbxt3WC24yvg/exec'; // لینک درستت رو جایگزین کن
+// آدرس‌های جدا برای هر Google Apps Script
+const NON_REF_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwVtOq8vLhEh_rkH8vsZ7-TugpHQGHffNOyZM3HtB8VkmXtAw3kxuETgwMbxt3WC24yvg/exec';
+const REF_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby11q80RS8PFy-g6eOiz6e0OeKtL0kkk_HAa-b0d3hRPuA1P3_rak2VFMoqq2kptji85Q/exec';
 
 app.post('/proxy', async (req, res) => {
   try {
-    const response = await fetch(GOOGLE_SCRIPT_URL, {
+    const data = req.body;
+
+    let targetUrl;
+    if (data.referrer_id) {
+      // اگر referrer_id داریم → یعنی درخواست مربوط به referral است
+      targetUrl = REF_SCRIPT_URL;
+    } else {
+      // در غیر اینصورت → non ref
+      targetUrl = NON_REF_SCRIPT_URL;
+    }
+
+    const response = await fetch(targetUrl, {
       method: 'POST',
-      body: JSON.stringify(req.body),
+      body: JSON.stringify(data),
       headers: { 'Content-Type': 'application/json' },
     });
+
     const text = await response.text();
     res.send(text);
   } catch (err) {
